39cdda35e25ee1f338061f1fb4e3bc5db1477895
kaitlyncliu <liu.kaitlync@gmail.com>
2022-07-19 22:44:07 -0400

    Finish sign in function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,29 @@ import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
 import { useContext } from 'react';
 import CartScreen from './screens/CartScreen';
 import SignInScreen from './screens/SignInScreen';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-	const { state } = useContext(Store);
-	const { cart } = state;
+	const { state, dispatch: ctxDispatch } = useContext(Store);
+	const { cart, userInfo } = state;
+
+	const signOutHandler = () => {
+		ctxDispatch({ type: 'USER_SIGNOUT' });
+		localStorage.removeItem('userInfo');
+	};
+
 	return (
 		<BrowserRouter>
 			<div className="d-flex flex-column site-container">
+				<ToastContainer position="bottom-center" limit={1} />
 				<header>
 					<Navbar bg="dark" variant="dark">
 						<Container>
@@ -32,6 +42,28 @@ function App() {
 										</Badge>
 									)}
 								</Link>
+								{userInfo ? (
+									<NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+										<LinkContainer to="/profile">
+											<NavDropdown.Item>User Profile</NavDropdown.Item>
+										</LinkContainer>
+										<LinkContainer to="/orders">
+											<NavDropdown.Item>My Orders</NavDropdown.Item>
+										</LinkContainer>
+										<NavDropdown.Divider />
+										<Link
+											className="dropdown-item"
+											to="#signout"
+											onClick={signOutHandler}
+										>
+											Sign Out
+										</Link>
+									</NavDropdown>
+								) : (
+									<Link className="nav-link" to="/signin">
+										Sign In
+									</Link>
+								)}
 							</Nav>
 						</Container>
 					</Navbar>
